4379eae0c6b7f07cf394cf44dc556d6dbc5e7a82
confident-architect-8379 <s.kanagui@gmail.com>
2020-08-16 23:06:17 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,7 +6,8 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh
+def assembled_aoh [
+  ]
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
 end
